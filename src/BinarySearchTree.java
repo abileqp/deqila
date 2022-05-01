82d47bdcd6e8e6eceaa2233db63c9408a80d9664
abileqp <102915042+abileqp@users.noreply.github.com>
2022-05-01 23:11:07 +0600

    Update BinarySearchTree.java

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,2 +1,39 @@
 public class BinarySearchTree {
+  void insert(int data) {
+   struct node *tempNode = (struct node*) malloc(sizeof(struct node));
+   struct node *current;
+   struct node *parent;
+
+   tempNode->data = data;
+   tempNode->leftChild = NULL;
+   tempNode->rightChild = NULL;
+
+   if(root == NULL) {
+      root = tempNode;
+   } else {
+      current = root;
+      parent = NULL;
+
+      while(1) {                
+         parent = current;
+			
+         if(data < parent->data) {
+            current = current->leftChild;
+				
+            if(current == NULL) {
+               parent->leftChild = tempNode;
+               return;
+            }
+         } 
+         else {
+            current = current->rightChild;
+
+            if(current == NULL) {
+               parent->rightChild = tempNode;
+               return;
+            }
+         }
+      }            
+   }
+}  
 }
