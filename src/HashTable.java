d5c9eeb30a3d8c27422c2631291340c59aed60a7
abileqp <102915042+abileqp@users.noreply.github.com>
2022-05-02 21:15:36 +0600

    Update HashTable.java

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,18 @@
-public class HashTable {
-}
+import java.util.*; 
+
+public class HashTable { 
+  public static void main(String args[]) 
+  {
+  Hashtable<Integer, Integer> 
+    ht = new Hashtable<Integer, Integer>(); 
+  
+  ht.put(123, 432); 
+  ht.put(12, 2345);
+  ht.put(15, 5643); 
+  ht.put(3, 321);
+
+  ht.remove(12);
+
+  System.out.println(ht); 
+  } 
+} 
